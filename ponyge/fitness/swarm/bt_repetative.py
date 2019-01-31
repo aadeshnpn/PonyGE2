49d37ca30129307664ba214b5045c1522d323047
Aadesh Neupane <neupaneaadesh@gmail.com>
2019-01-30 17:52:25 -0700

    new function added to compare performance againts the diversity function

--- ponyge/fitness/swarm/bt_repetative.py
+++ ponyge/fitness/swarm/bt_repetative.py
@@ -1,15 +1,15 @@
-"""Behavior Diversity classes for swarms."""
+"""Behavior repetative classes for swarms."""
 
 import math
 from ponyge.fitness.base_ff_classes.base_ff import base_ff
 import xml.etree.ElementTree as ET
 
 
-class bt_diversity(base_ff):
+class bt_repetative(base_ff):
     """
     Calculation the fintness of the individual.
     As the objective fitness is not yet avaliable, we calcualte fitness
-    based on the diversity of the BT evaluated for itself
+    based on the repetative structure of the BT evaluated for itself.
     """
 
     maximise = True  # True as it ever was.
@@ -17,13 +17,6 @@ class bt_diversity(base_ff):
     def __init__(self, parameter):
         # Initialise base fitness function class.
         super().__init__()
-        """
-        self.execution_behaviors = ['IsCarryable', 'IsSingleCarry',
-        'SingleCarry', 'NeighbourObjects', 'IsMultipleCarry',
-        'IsInPartialAttached', 'InitiateMultipleCarry',
-        'IsEnoughStrengthToCarry', 'Move', 'GoTo',
-        'IsMotionTrue', 'RandomWalk', 'IsMoveable', 'MultipleCarry']
-        """
         self.execution_behaviors = [
             'MoveTowards', 'Explore', 'CompositeSingleCarry',
             'CompositeDrop', 'MoveAway', 'NeighbourObjects', 'IsDropable',
@@ -45,32 +38,13 @@ class bt_diversity(base_ff):
     def calcualte_diversity(self):
         self.sorted_keys = list(self.execution.keys())
         self.sorted_keys.sort()
-        self.sorted_values = list(self.execution.values())
-        self.sorted_values.sort()
-        new_execution = dict()
-        sorted_values_sum = sum(self.sorted_values)
-        behavior_len = len(self.execution_behaviors)
-        divisor = math.ceil(sorted_values_sum / behavior_len) * behavior_len
-        if self.sorted_keys == self.execution_behaviors and \
-                sorted_values_sum % behavior_len == 0 and \
-                self.sorted_values[0] == int(
-                    sorted_values_sum / behavior_len):
-                    diversity = 1
-        elif self.sorted_keys == self.execution_behaviors and \
-                self.sorted_values[0] <= int(
-                    sorted_values_sum / behavior_len):
-                    for a in self.execution.keys():
-                        self.execution[a] -= self.sorted_values[0]
-                        if self.execution[a] > 0:
-                            new_execution[a] = self.execution[a]
+        counter = 0.0
+        three_main_nodes = ['Explore', 'CompositeSingleCarry', 'CompositeDrop']
+        for node in three_main_nodes:
+            if node in self.sorted_keys:
+                counter += 1
 
-                    other_match_count = self.other_match_value(new_execution)
-                    diversity = (self.sorted_values[0] * behavior_len + other_match_count * 1.0) / divisor
-        else:
-            other_match_count = self.other_match_value(self.execution)
-            diversity = (other_match_count * 1.0) / divisor
-
-        return diversity * 100
+        return (counter / 3.0) * 100
 
     def other_match_value(self, exection):
         match_set = set(exection.keys()) & set(self.execution_behaviors)
@@ -91,6 +65,7 @@ class bt_diversity(base_ff):
         self.control['Sequence'] = 0
         self.control['Selector'] = 0
         self.execution = dict()
+        depth = 0
         for node in self.root.iter():
             if node.tag in ['Sequence', 'Selector']:
                 self.control[node.tag] += 1
